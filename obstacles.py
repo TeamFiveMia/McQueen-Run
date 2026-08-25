2be68c5efe8e0bd8dcd94360c69ea762f9ecea2d
MahmoudSalama07 <mahmoudsalama.personal@gmail.com>
2026-08-25 16:40:13 +0300

    added tire subclass

--- obstacles.py
+++ obstacles.py
@@ -9,6 +9,28 @@ class Item:
         self.bottom_position = bottom_pos
         self.active = True
 
+    def step(self):
+        # If the block hits the bottom, destroy it, else move it down.
+        if (self.position <= self.bottom_pos):
+            self.active = False
+
+        else:
+            self.position = self.position - self.speed
+
+
+
+
+        
+class Tire(Item):
+    def __init__(self, lane, speed, top_pos, bottom_pos):
+        super().__init__(lane, speed, top_pos, bottom_pos)
+        
+    # If collided, decrease points
+    def collided(self, points):
+        ...
+        return points
+
+
 
 
 
