ad5fb50613be7012a5c9bc4ffa9fcbd1cdccd1a7
MahmoudSalama07 <mahmoudsalama.personal@gmail.com>
2026-08-25 16:41:32 +0300

    added functions file with plan comments

---
+++ functions.py
@@ -0,0 +1,21 @@
+import obstacles as ob
+import random as rand
+import cv2 as cv
+
+speed = 5
+top_pos = 200
+bottom_pos = 0
+number = 10
+lanes = 4
+
+
+# Populate: Returns a list of random items in random lanes
+...
+# Replace inactive items
+...
+# Check Position for any object (returns lane and pos)
+...
+# Tire: If Position == McQueen's and Vulnerability == True: Lose Nitro Points
+...
+# Nitro: If Position == McQueen's and Vulnerability == True: Gain Nitro Points
+...
