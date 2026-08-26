8d65a557c272deb6061844aeb1fa4600feb27a95
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-08-26 22:01:18 +0300

    detection call

--- useNitro.py
+++ useNitro.py
@@ -3,8 +3,17 @@ nitro = 0
 valnerable  = False
 boost = False
 boost_time = 2
+currentTime = 0
 def nitro_add():
     global nitro
     nitro+=1
 def after_detection():
-    
+    global nitro,valnerable,boost,boost_time
+    if nitro > 0 and not boost:
+        nitro -=1
+        valnerable = False
+        boost = True
+        currentTime = time.time() + boost_time
+def response():
+    global valnerable,boost
+        
