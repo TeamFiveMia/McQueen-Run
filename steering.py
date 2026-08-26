92261a63feacdf0c6031daf58e0e4533ee5ad1aa
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-26 13:39:35 +0300

    Define a function to estimate the car position (centered in the lane)

--- steering.py
+++ steering.py
@@ -7,6 +7,12 @@ def main():
    ...
 
 
+def calc_position(lane, frame_width, no_lanes):
+
+    # Calculate lane width
+    lane_width = frame_width / no_lanes
+    # Calculate the Car Position
+    position = no_lanes * lane + lane_width / 2
 
 
 def get_lane(x_center, frame_width, no_lanes):
@@ -15,9 +21,9 @@ def get_lane(x_center, frame_width, no_lanes):
     lane_width = frame_width / no_lanes
     # Return the index of the lane
     lane = int((x_center / lane_width))
-    # Solve the problem of the last lane
+    # Ensure that lane is not greater than last valid lane
     lane = min(lane, no_lanes - 1)
-    
+
     return lane  
 
 if __name__ == "__main__":
