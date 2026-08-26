da884566997a1d6324d0bf48d5d3ae802e08bc2a
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-08-26 22:21:36 +0300

    updated gitignore

--- currentGameState.py
+++
@@ -1,7 +0,0 @@
-class GameState:
-    def __init__(self, lane, speed, top_pos, bottom_pos):
-        self.speed = speed
-        self.lane = lane
-        self.position = top_pos + rand.randint(1, 100)
-        self.bottom_pos = bottom_pos
-        self.active = True
