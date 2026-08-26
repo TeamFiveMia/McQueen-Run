a37133b246df412af6da535ce836e75f05d290de
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-26 13:25:21 +0300

    Modifying get_lane function to overcome boundary problems

--- steering.py
+++ steering.py
@@ -14,7 +14,11 @@ def get_lane(x_center, frame_width, no_lanes):
     # Calculate the lane width
     lane_width = frame_width / no_lanes
     # Return the index of the lane
-    return int((x_center / lane_width) - 1)
+    lane = int((x_center / lane_width))
+    # Solve the problem of the last lane
+    lane = min(lane, no_lanes - 1)
+    
+    return lane  
 
 if __name__ == "__main__":
     main()
