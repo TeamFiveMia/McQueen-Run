7f7f13f398e8279e504e49f4b1ec6ef21fa9f9bd
MahmoudSalama07 <mahmoudsalama.personal@gmail.com>
2026-08-26 16:11:33 +0300

    added game logic and collision methods

--- obstacles.py
+++ obstacles.py
@@ -1,23 +1,29 @@
-
+import random as rand
 
 # Define a class "Item" which is any item that appears in the game
 class Item:
     def __init__(self, lane, speed, top_pos, bottom_pos):
         self.speed = speed
         self.lane = lane
-        self.start_position = top_pos
-        self.bottom_position = bottom_pos
+        self.position = top_pos + rand.randint(1, 100)
+        self.bottom_pos = bottom_pos
         self.active = True
 
-    def step(self):
-        # If the block hits the bottom, destroy it, else move it down.
+    def step(self): # Steps the item
+        # If the item hits the bottom, destroy it, else move it down.
         if (self.position <= self.bottom_pos):
             self.active = False
 
         else:
             self.position = self.position - self.speed
 
-
+    def collided(self, lane, mcqueen, vulnerable): # Checks if the item has collided
+        # If vulnerable (not using Nitro), in the same lane and same position as McQueen, then it collides
+        if vulnerable == True:
+            if self.lane == lane:
+                if self.position == mcqueen:
+                    return True
+        return False
 
 
         
@@ -25,9 +31,10 @@ class Tire(Item):
     def __init__(self, lane, speed, top_pos, bottom_pos):
         super().__init__(lane, speed, top_pos, bottom_pos)
         
-    # If collided, decrease points
-    def collided(self, points):
-        ...
+    # If collided, decrease points and deactivate
+    def collision_action(self, points, penalty):
+        points = points - penalty
+        self.active = False
         return points
 
 
@@ -38,9 +45,10 @@ class Nitro(Item):
     def __init__(self, lane, speed, top_pos, bottom_pos):
         super().__init__(lane, speed, top_pos, bottom_pos)
 
-    # If collided, increase points and disappear
-    def collided(self, points):
-        ...
+    # If collided, increase points and deactivate
+    def collision_action(self, points, reward):
+        points = points + reward
+        self.active = False
         return points
 
 
