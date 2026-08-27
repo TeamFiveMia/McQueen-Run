7e2181bed095e1c87a9c5c90dee25a3df6c87e2c
Omar Samy <omar3abqarino@gmail.com>
2026-08-27 14:56:33 +0300

    .

--- yolo model/src/autolabeler.py
+++ yolo model/src/autolabeler.py
@@ -1,11 +1,16 @@
 import os
 import cv2 as cv
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+
+CLASS_MAP = {"Palm": 0, "Peace Sign": 1}
 
 input_dir = "data\\data\\train"
 output = "data\\labels"
 os.makedirs(output, exist_ok=True)
-
+test_pic = "extracted_frames\\frame_00001.jpg"
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
@@ -14,17 +19,23 @@ hands = mp_hands.Hands(
     min_detection_confidence=0.5
 )
 
-for file in os.listdir(output):
-    if not file.lower().endswith(('.png', '.jpg', '.jpeg')):
-        continue
+# for file in os.listdir(input_dir):
+with test_pic as file:
+    # if not file.lower().endswith(('.png', '.jpg', '.jpeg')):
+    #     continue
 
-    path_img = os.path.join(input_dir, file)
-    img = cv.imread(path_img, cv.IMREAD_COLOR_RGB)
+    # path_img = os.path.join(input_dir, file)
+    # label
 
-    if not img:
-        continue
+    img = cv.imread(file, cv.IMREAD_COLOR_RGB)
+
+    # if not img:
+    #     return
 
     height, width, _ = img.shape
 
-    
+    hand_results = hands.process(img)
+    print(hand_results)
+
+
 
