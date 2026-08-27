5cc81032dbb8306cefaba17df11cd00635b3806f
Omar Samy <omar3abqarino@gmail.com>
2026-08-27 14:39:37 +0300

    added the trainer file, img preparation for mediapipe autolabeling

--- yolo model/src/autolabeler.py
+++ yolo model/src/autolabeler.py
@@ -1,10 +1,10 @@
 import os
-import cv2
+import cv2 as cv
 import mediapipe as mp
 
 input_dir = "data\\data\\train"
-OUTPUT_DIR = "data\\labels"
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+output = "data\\labels"
+os.makedirs(output, exist_ok=True)
 
 
 mp_hands = mp.solutions.hands
@@ -13,3 +13,18 @@ hands = mp_hands.Hands(
     max_num_hands=2, 
     min_detection_confidence=0.5
 )
+
+for file in os.listdir(output):
+    if not file.lower().endswith(('.png', '.jpg', '.jpeg')):
+        continue
+
+    path_img = os.path.join(input_dir, file)
+    img = cv.imread(path_img, cv.IMREAD_COLOR_RGB)
+
+    if not img:
+        continue
+
+    height, width, _ = img.shape
+
+    
+
